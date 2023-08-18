eb54b014f34bc7499d28a07dc1faeadf84082ba4
Diptodeep <87961483+TheLegenDev@users.noreply.github.com>
2023-08-18 12:02:52 +0530

    Update ping.js

--- src/commands/Miscellaneous/ping.js
+++ src/commands/Miscellaneous/ping.js
@@ -1,43 +1,70 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
- 
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription(`Check the bot's latency.`),
+    .setName('ping')
+    .setDescription(`Check the bot's latency.`),
     
-        async execute(interaction) {
+    async execute(interaction, client) {
         let circles = {
             good: '<:high:1141959052289638440>',
             okay: '<:mid:1141959164453736479>',
             bad: '<:low:1141959216383402044>',
         };
- 
-        await interaction.deferReply();
- 
-        const pinging = await interaction.editReply({ content: 'Obtaining ping...' });
- 
+
         const ws = interaction.client.ws.ping;
-        const msgEdit = Date.now() - pinging.createdTimestamp;
+        const msgEdit = Date.now() - interaction.createdTimestamp;
  
         const wsEmoji = ws <= 100 ? circles.good : ws <= 200 ? circles.okay : circles.bad;
         const msgEmoji = msgEdit <= 200 ? circles.good : circles.bad;
- 
-        const pingEmbed = new EmbedBuilder()
-            .setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
-            .setColor('Blue')
-            .setTimestamp()
-            .setFooter({ text: `Pinged At` })
-            .addFields(
-                {
-                    name: 'Websocket Latency',
-                    value: `${wsEmoji} \`${ws}ms\``,
-                },
-                {
-                    name: 'API Latency',
-                    value: `${msgEmoji} \`${msgEdit}ms\``,
-                }
-            );
- 
-        await pinging.edit({ embeds: [pingEmbed], content: '\u200b' });
-    },
-};
+
+        const embed = new EmbedBuilder()
+        .setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
+        .setColor('#2f3136')
+        .setTimestamp()
+        .setFooter({ text: `Latency Recorded` })
+        .addFields(
+            {
+                name: 'Websocket Latency',
+                value: `${wsEmoji} \`${ws}ms\``,
+            },
+            {
+                name: 'API Latency',
+                value: `${msgEmoji} \`${msgEdit}ms\``,
+            }
+        );
+
+        const btn = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+            .setCustomId('btn')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('🔁')
+        )
+
+        const msg = await interaction.reply({ embeds: [embed], components: [btn] })
+
+        const collector = msg.createMessageComponentCollector()
+        collector.on('collect', async i => {
+            if(i.customId == 'btn') {
+                i.update({ embeds: [
+                    new EmbedBuilder()
+                    .setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
+                    .setColor('#2f3136')
+                    .setTimestamp()
+                    .setFooter({ text: `Latency Recorded` })
+                    .addFields(
+                        {
+                            name: 'Websocket Latency',
+                            value: `${wsEmoji} \`${ws}ms\``,
+                        },
+                        {
+                            name: 'API Latency',
+                            value: `${msgEmoji} \`${msgEdit}ms\``,
+                        }
+                    )
+                ], components: [btn] })
+            }
+        })
+    }
+}
